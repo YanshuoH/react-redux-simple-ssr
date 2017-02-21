ede13fb343c6c6effc363440973a94298485291e
Yanshuo H <huangyanshuo@meican.com>
2017-02-22 07:41:19 +0800

    client webpack server

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const webpackConfig = {
+  entry: {
+    client: './client/index',
+    vendor: ['react'],
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].dist.js',
+  },
+  devServer: {
+    inline: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'client'),
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'client/index.html'),
+      hash: true,
+    }),
+  ],
+  devtool: 'source-map',
+}
+
+module.exports = webpackConfig;
